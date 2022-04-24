3c3ae499209ad77e4cbdaf1b1c1cbb9145a2fadc
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-04-24 16:57:51 -0500

    Hardcoded plus and minus signs from screenshots

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -1,36 +1,42 @@
-import React from 'react'
-import {useState} from 'react'
-import Button from "../button"
+import React from "react";
+import { useState } from "react";
+import Button from "../button";
 import * as css from "./css.module.scss";
+import minusButton from "../../assets/images/minusButton.png"
+import plusButton from "../../assets/images/plusButton.png"
 
+const Team = ({ name, ID }) => {
+  //keeps track of score with state variables
+  //increment and decrement score on button click
+  //Dsplays team names, score, and + and - buttons
 
-const Team = ({name,ID}) =>{
-//keeps track of score with state variables
-//increment and decrement score on button click
-//Dsplays team names, score, and + and - buttons
+  const [score, setScore] = useState(0);
 
-const [score,setScore] = useState(0);
+  const incrementScore = () => {
+    setScore(score + 1);
+  };
 
-const incrementScore = ()=>{
-    setScore(score+1);
-}
+  const decrementScore = () => {
+    setScore(score - 1);
+  };
 
-const decrementScore = () =>{
-    setScore(score -1);
-}
-    
-    return(
-        <div style = {{borderColor: 'black' ,borderStyle: "solid",}} class = {css.divStyle} >
-            <p className = {css.p}>{name}</p>
-            <h3 style = {{textAlign: "center"}}>{score}</h3>
-            <div id = "scoreButtonDiv" class = {css.buttonDivStyle}>
-                <Button  type = "roundedEdges" onClick = {()=> decrementScore()}> - </Button>
-                <Button  type = "roundedEdges" onClick = {()=> incrementScore()}> + </Button>
+  return (
+    <div
+      style={{ borderColor: "black", borderStyle: "solid" }}
+      class={css.divStyle}
+    >
+      <p className={css.p}>{name}</p>
+      <h3 style={{ textAlign: "center" }}>{score}</h3>
+      <div id="scoreButtonDiv" class={css.buttonDivStyle}>
+        <Button type="roundedEdges" onClick={() => decrementScore()}>
+          <img  className = {css.scoreButtonDivImg} src={minusButton}  />{" "}
+        </Button>
+        <Button type="roundedEdges" onClick={() => incrementScore()}>
+          <img className = {css.scoreButtonDivImg} src ={plusButton} />
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-          
-        </div>
-    )
-}
-
-export default Team
+export default Team;
