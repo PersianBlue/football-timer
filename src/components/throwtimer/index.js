bd345e4b42382e7be47ac76f433e719efdc404cd
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-05-17 21:44:45 -0500

    Added ChangeTeamNames button

--- src/components/throwtimer/index.js
+++ src/components/throwtimer/index.js
@@ -1,68 +1,76 @@
-import React from 'react';
-import Button from "../button"
-import {useState,useEffect} from 'react';
-import whistleSound from "../../assets/audio/WhistleSoundEffect.mp3"
+import React from "react";
+import Button from "../button";
+import { useState, useEffect } from "react";
+import whistleSound from "../../assets/audio/WhistleSoundEffect.mp3";
 import * as css from "./css.module.scss";
 
-const ThrowTimer = ()=>{
-    //exports changeSeconds and countdownTimer buttons
-    //counter keeps track of the countdown timer
-    //newCounter is used to set the counter to a new variable with
-    //the changeSeconds button
-    //runCountdown acts as a switch to trigger the countdown after button click
+const ThrowTimer = () => {
+  //exports changeSeconds and countdownTimer buttons
+  //counter keeps track of the countdown timer
+  //newCounter is used to set the counter to a new variable with
+  //the changeSeconds button
+  //runCountdown acts as a switch to trigger the countdown after button click
 
-    const[counter,setCounter] = useState(4);
-    const[runCountdown, setRunCountdown] = useState(false);
-    const[newCounter, setNewCounter] = useState(4);
+  const [counter, setCounter] = useState(4);
+  const [runCountdown, setRunCountdown] = useState(false);
+  const [newCounter, setNewCounter] = useState(4);
 
-    const startCountdown = ()=>{
-        setRunCountdown(true);
+  const startCountdown = () => {
+    setRunCountdown(true);
+  };
+  useEffect(
+    //useEffect deals with infinite render problems
+    () => {
+      if (runCountdown && counter > 0) {
+        const handle = setInterval(() => setCounter(counter - 1), 1000);
+        return () => clearInterval(handle);
+      } else {
+        setRunCountdown(false);
+        setCounter(newCounter);
+      }
+      if (counter === 0) {
+        //play soundeffect when countdown ends
+        var audio = new Audio(whistleSound);
+        audio.play();
+      }
+    },
+    [runCountdown, newCounter, counter] //adding dependencies to avoid infinite updates
+  );
+
+  const setSeconds = () => {
+    //creates a prompt for changing countdown timer length
+    //if statement checks validity of input
+    let seconds = window.prompt("Enter seconds here");
+    if (Number.isNaN(seconds) || isNaN(seconds) || seconds === "") {
+      seconds = counter;
     }
-    useEffect( //useEffect deals with infinite render problems
-        ()=>{
-            if(runCountdown && counter>0){
-                const handle = setInterval(()=>setCounter(counter-1),1000)
-                return ()=>clearInterval(handle);
+    seconds = parseInt(seconds);
+    return setNewCounter(seconds);
+  };
 
-            }else{
-                setRunCountdown(false);
-                setCounter(newCounter);
-            }
-            if(counter===0){ //play soundeffect when countdown ends
-                var audio = new Audio(whistleSound)
-                audio.play();
-            }
-        },[runCountdown,newCounter,counter] //adding dependencies to avoid infinite updates
-    )
-    
-    const setSeconds = ()=>{
-        //creates a prompt for changing countdown timer length
-        //if statement checks validity of input
-        let seconds = window.prompt("Enter seconds here");
-        if(Number.isNaN(seconds) || isNaN(seconds) || seconds===""){
-            seconds = counter;
-        }
-        seconds = parseInt(seconds);
-        return setNewCounter(seconds);
-    }
-  
+  return (
+    //produces the changeSeconds and countdownTimer button
+    <div id="throwTimerDiv">
+      <br></br>
+      <div className={css.rightAlign}>
+        <Button name="changeSeconds" onClick={() => setSeconds()}>
+          Change Seconds
+        </Button>
+      </div>
 
-    return (
-        //produces the changeSeconds and countdownTimer button
-        <div id ="throwTimerDiv">
-            <br></br>
-            <div className = {css.rightAlign}>
-                 <Button name = "changeSeconds" onClick= {()=>setSeconds()}>Change Seconds</Button>
-            </div>
-            
-           <div  className ={css.centerAlign}> 
-               <Button id="countdownTimer" name = "countdownTimer" type = "circular" onClick ={()=>startCountdown()}> {counter}</Button>
-           </div>
-            
-            
-        </div>
-    )
+      <div className={css.centerAlign}>
+        <Button
+          id="countdownTimer"
+          name="countdownTimer"
+          type="circular"
+          onClick={() => startCountdown()}
+        >
+          {" "}
+          {counter}
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default ThrowTimer
+export default ThrowTimer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,47 @@
 import React from "react";
 import Button from "../components/button";
 import GameClock from "../components/gameclock";
-import SignInButton from "../components/SignInButton/signInButton";
 import SignInPage from "../components/SignInButton/signInPage";
 import Team from "../components/team";
 import ThrowTimer from "../components/throwtimer";
 import "../global.scss";
+import { useState } from "react";
 
 const App = (props) => {
+  const [teamNames, setTeamNames] = useState([
+    {
+      teamOne: "Team One",
+    },
+    { teamTwo: "Team Two" },
+  ]);
+
+  const changeNames = () => {
+    let name1 = null;
+    let name2 = null;
+    name1 = window.prompt("Enter the name for Team 1");
+    name2 = window.prompt("Enter the name for Team 2");
+    let arr = [...teamNames];
+    if (name1 != "") {
+      arr[0] = { teamOne: name1 };
+    }
+    if (name2 != "") {
+      arr[1] = { teamTwo: name2 };
+    }
+    setTeamNames(arr);
+    console.log("Name1 :", name1);
+    console.log("Team one: ", teamNames[0]);
+    console.log("Name2 :", name2);
+    console.log("Team Two: ", teamNames[1]);
+  };
+
   return (
     <main className="body">
       <div style={{ width: 350 }}>
         <GameClock />
+        <Button onClick={() => changeNames()}>Change Team Names </Button>
         <SignInPage />
-        <Team name="Team One" id={1} />
-        <Team name="Team Two" id={2} />
+        <Team name={teamNames[0].teamOne} id={1} />
+        <Team name={teamNames[1].teamTwo} id={2} />
         <ThrowTimer />
       </div>
     </main>
