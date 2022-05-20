0fbe50dffd39152eda23360e72596fdc08096aca
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-05-19 22:52:01 -0500

    Fixed Google Sign In
    Lightbulb moment told me I reinstalled firebase in order to use firestore, and that's when SSO stopped working via firebase authentication. Stopped the development server and restarted it, to reflect the changes, and now the SSO works again. Lovely.

--- src/components/database/readFromDatabase.js
+++ src/components/database/readFromDatabase.js
@@ -1,19 +1,19 @@
-// import React from "react";
-// import { auth, db } from "../../firebase-config";
-// import {
-//   collection,
-//   addDoc,
-//   serverTimestamp,
-//   getDoc,
-//   getDocs,
-// } from "firebase/firestore";
+import React from "react";
+import { auth, db } from "../../firebase-config";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 
-// async function ReadFromDatabase() {
-//   console.log("Reading from Database");
-//   const querySnapshot = await getDocs(collection(db, "users"));
-//   querySnapshot.forEach((doc) => {
-//     console.log(`${doc.id} => ${doc.data()}`);
-//   });
-// }
+async function ReadFromDatabase() {
+  console.log("Reading from Database");
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+}
 
-// export default ReadFromDatabase;
+export default ReadFromDatabase;
