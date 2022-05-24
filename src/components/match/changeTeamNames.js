5b0865f291e3b6e816664be87554529baea3c04b
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-05-24 17:09:15 -0500

    Bug fix changeTeamNames
    Before, if you cancelled the window prompts, the team names would get changed to nothing and minimize (because null was being returned but we only checked for empty string)
    
    Adjusted the conditional logic to just check if the values entered were truthy which solved the problem

--- src/components/match/changeTeamNames.js
+++ src/components/match/changeTeamNames.js
@@ -7,22 +7,20 @@ import Button from "../button";
 const ChangeTeamNames = ({ teamNames, setTeamNames }) => {
   console.log("Rendering ChangeTeamNames");
 
-
   const changeNames = () => {
     let name1 = null;
     let name2 = null;
     name1 = window.prompt("Enter the name for Team 1");
     name2 = window.prompt("Enter the name for Team 2");
     let arr = [...teamNames];
-    if (name1 != "") {
+    if (name1) {
       arr[0] = { teamOne: name1 };
     }
-    if (name2 != "") {
+    if (name2) {
       arr[1] = { teamTwo: name2 };
     }
     setTeamNames(arr);
   };
-
 
   return (
     <div>
